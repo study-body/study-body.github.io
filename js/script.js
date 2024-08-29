55d049ab9912d8594878bfe6c689a4d49ca3ea42
study-body <petkapipa777@gmail.com>
2024-08-29 17:56:59 +0300

    push

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let addCardButton = document.getElementById('addCardButton')
+let generatorForm = document.getElementById('generatorForm')
+
+function show() {
+    generatorForm.style = 'visibility: visible;'
+}
+
